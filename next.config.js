3e693d1cc904aa0dd9233eb0475e8dd99d0ff2c4
barbosa <eurisonfbarbosa@gmail.com>
2023-03-24 13:48:43 -0300

    :rocket: frist commit in ignite shop

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      "files.stripe.com",
+    ]
+  }
+
+  // this.experimental: {
+  //   images: {
+  //     allowFutureImage: true,
+  //   }
+  // }
 }
 
 module.exports = nextConfig
